84f726a8f8819f44f0722d9c732fcb87abc33137
Noni86kg <nonisc2@gmail.com>
2021-10-02 19:17:31 +0200

    Zadatak 4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const APIURL = 'https://api.github.com/users/'
 const toggle = document.querySelector('.toggle');
 const darkLight = document.querySelector('.dark-light');
 const moonIcon = document.querySelector('.moon-light-bg');
@@ -9,6 +10,28 @@ const h2 = document.querySelector('h2');
 const main = document.querySelector('main');
 const number = document.querySelector('.number');
 const icons = document.querySelector('.icons');
+const button = document.querySelector('button');
+const form = document.getElementById('form')
+const errorMessage = document.querySelector('.error-search');
+
+const userName = document.getElementById('user-name');
+const userLink = document.getElementById('link');
+const date = document.getElementById('date');
+const lorem = document.getElementById('lorem');
+const userImg = document.getElementById('user-img');
+
+const repos = document.getElementById('repos');
+const followers = document.getElementById('followers');
+const following = document.getElementById('following');
+
+const city = document.getElementById('city');
+const locationClass = document.querySelector('.location');
+const twitter = document.getElementById('twitter');
+const twitterClass = document.querySelector('.twitter');
+const website = document.getElementById('website');
+const websiteClass = document.querySelector('.website');
+const company = document.getElementById('company');
+const companyClass = document.querySelector('.company');
 
 // dark , light mode
 toggle.addEventListener('click', function(e) {
@@ -29,4 +52,59 @@ toggle.addEventListener('click', function(e) {
     number.classList.toggle('active');
     icons.classList.toggle('active');
     search.classList.toggle("placeholder-red");
+})
+
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username)
+        renderData(data)
+    } catch(err) {
+        if(err.response.status == 404) {
+            errorMessage.classList.add('active');
+        }
+
+    }
+}
+
+function renderData(data) {
+    userName.textContent = data.login;
+    userLink.textContent = `@${data.login}`;
+    date.textContent = `Joined ${data.created_at.slice(0,10)}`;
+    lorem.textContent = data.bio ? data.bio : `This profile has no bio`;
+    userImg.setAttribute('src', data.avatar_url)
+
+    repos.textContent = data.public_repos;
+    followers.textContent = data.followers;
+    following.textContent = data.following;
+
+    city.textContent = data.location ? data.location : `Not Available`;
+    if(city.textContent == `Not Available`) {
+        locationClass.style.opacity="0.3";
+    }
+    twitter.textContent = data.twitter_username ? data.twitter_username : `Not Available`;
+    if(twitter.textContent == `Not Available`) {
+        twitterClass.style.opacity="0.3";
+    }
+    website.textContent = data.html_url.slice(8, data.html_url.length);
+    if(website.textContent == `Not Available`) {
+        websiteClass.style.opacity="0.3";
+    }
+    company.textContent = data.company ? data.company : `Not Available`;
+    if(company.textContent == `Not Available`) {
+        companyClass.style.opacity="0.3";
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    errorMessage.classList.remove('active');
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
 })
